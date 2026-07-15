4b8758920364394e3a52626579e170d1c2aad501
Ahmed Ramadan <elgen9961@gmail.com>
2026-07-15 18:49:49 +0300

    feat: create reusable handlerFactory for controller CRUD operations

---
+++ server/controllers/handlerFactory.js
@@ -0,0 +1,63 @@
+import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/appError.js';
+import APIFeatures from '../utils/apiFeatures.js';
+
+export const getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .pagination();
+
+    const docs = await features.query;
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: docs.length,
+      data: {
+        data: docs,
+      },
+    });
+  });
+
+export const createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: { doc },
+    });
+  });
+
+export const updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(200).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  });
+
+export const deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    // 204 => means no content
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
