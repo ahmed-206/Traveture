68d018c1dbf3736bea9ddeafd8053900adcf16cb
Ahmed Ramadan <elgen9961@gmail.com>
2026-07-29 20:07:07 +0300

    feat: add advanced query features with pagination metadata
    - add totalCount support to list responses
    
    - calculate filtered document count for pagination
    
    - improve handlerFactory getAll
    
    - enhance sendResponse utility to include totalCount

--- server/utils/apiFeatures.js
+++ server/utils/apiFeatures.js
@@ -5,12 +5,16 @@ class APIFeatures {
   }
 
   filter() {
+    
     const queryObj = { ...this.queryString };
+
     const excludedFields = ['sort', 'page', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObj[el]);
 
+  
     let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt|regex|options)\b/g, (match) => `$${match}`);
+    
     this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
--- server/utils/sendResponse.js
+++ server/utils/sendResponse.js
@@ -1,8 +1,19 @@
-
-const sendResponse = (res, statusCode, { message, data, results } = {}) => {
+/**
+ * Sends a standardized JSON success response.
+ *
+ * @param {import('express').Response} res - Express response object.
+ * @param {number} statusCode - HTTP status code.
+ * @param {object} [options]
+ * @param {string} [options.message] - Human-readable success message.
+ * @param {*} [options.data] - Payload (single doc or array).
+ * @param {number} [options.results] - Total count (list endpoints only).
+ * @param {number} [options.totalCount]
+ */
+const sendResponse = (res, statusCode, { message, data, results,totalCount } = {}) => {
   const response = { status: 'success' };
   if (message) response.message = message;
   if (results !== undefined) response.results = results;
+  if (totalCount !== undefined) response.totalCount = totalCount;
   if (data !== undefined) response.data = data;
   res.status(statusCode).json(response);
 };
